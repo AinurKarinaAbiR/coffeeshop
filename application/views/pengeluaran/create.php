fdf61a66e24337cf5e78f297a30c89c284295504
oong26 <mkhalil26122000@gmail.com>
2022-06-19 20:59:33 +0700

    Master pengeluaran.

---
+++ application/views/pengeluaran/create.php
@@ -0,0 +1,33 @@
+<div id="menu" class="menu">
+	<div class="container">
+		<form action="<?= base_url('Pengeluaran/store') ?>" method="post">
+			<div class="row mt-5">
+				<div class="col-12">
+					<div class="row">
+						<div class="col-6">
+							<div class="form-group">
+								<label for="judul">Judul</label>
+								<input type="text" name="judul" id="judul" class="form-control" placeholder="Masukkan judul disini">
+							</div>
+						</div>
+						<div class="col-6">
+							<div class="form-group">
+								<label for="nominal">Nominal</label>
+								<input type="number" name="nominal" id="nominal" class="form-control" placeholder="Masukkan nominal disini">
+							</div>
+						</div>
+					</div>
+					<div class="row">
+						<div class="col-6">
+							<div class="form-group">
+								<label for="keterangan">Keterangan</label>
+								<textarea name="keterangan" id="keterangan" cols="30" rows="3" class="form-control"></textarea>
+							</div>
+						</div>
+					</div>
+					<input type="submit" value="Simpan" class="btn btn-primary">
+				</div>
+			</div>
+		</form>
+	</div>
+</div>
---
+++ application/views/pengeluaran/index.php
@@ -0,0 +1,46 @@
+<div id="menu" class="menu">
+	<div class="container">
+		<a href="<?= base_url('Pengeluaran/create') ?>" class="btn btn-info my-4">Tambah</a>
+		<div class="table-responsive mt-4">
+			<table class="table">
+				<thead>
+					<tr>
+						<th>#</th>
+						<th>Judul</th>
+						<th>Keterangan</th>
+						<th>Nominal</th>
+						<th>Waktu</th>
+						<th>Aksi</th>
+					</tr>
+				</thead>
+				<tbody>
+					<?php $no = 1; ?>
+					<?php foreach ($data as $item) : ?>
+						<tr>
+							<td><?= $no ?></td>
+							<td><?= $item['judul'] ?></td>
+							<td><?= $item['keterangan'] ?></td>
+							<td>Rp <?= number_format($item['nominal'], 0, ',', '.') ?></td>
+							<td><?= $item['created_at'] ?></td>
+							<td>
+								<a href=" <?= base_url('Pengeluaran/delete/') . $item['id']; ?>">
+									<button class="btn btn-danger" onclick=" return confirm('Anda yakin akan menghapus menu ini?')">Hapus</button>
+								</a>
+							</td>
+						</tr>
+					<?php $no++;
+					endforeach; ?>
+				</tbody>
+				<tfoot>
+					<tr>
+						<th colspan="6">
+							<div class="float-right">
+								<?= $this->pagination->create_links(); ?>
+							</div>
+						</th>
+					</tr>
+				</tfoot>
+			</table>
+		</div>
+	</div>
+</div>
